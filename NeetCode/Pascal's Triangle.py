2f40de5bbfe9a0353d81be7f2a7163ef03410fc8
Abdelkarim Anouer <188693436+abdelkarimanouer@users.noreply.github.com>
2026-04-24 11:37:36 +0100

    Implement Pascal's Triangle generation

---
+++ NeetCode/Pascal's Triangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        t = []
+        for i in range(1, numRows + 1):
+            l = [1]
+            if t:
+                for j in range(len(t[-1]) - 1):
+                    l.append(t[-1][j] + t[-1][j + 1])
+                l.append(1)
+            t.append(l)
+        return t
