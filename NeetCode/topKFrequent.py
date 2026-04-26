9dd04b455675e409412df7fb6bd9f846ddd712cc
Abdelkarim Anouer <188693436+abdelkarimanouer@users.noreply.github.com>
2026-04-26 11:38:58 +0100

    Refactor topKFrequent for efficiency and clarity
    Refactor topKFrequent method to use dictionary get method and simplify sorting logic.

--- NeetCode/topKFrequent.py
+++ NeetCode/topKFrequent.py
@@ -2,11 +2,9 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         d = dict()
         l = []
+        c = 0
         for n in nums:
-            if n not in d.keys():
-                d[n] = nums.count(n)
-        s_d = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
-        print(s_d)
-        for i in range(k):
-            l.append(list(s_d.keys())[i])
-        return l
+            d[n] = d.get(n, 0) + 1
+
+        sorted_keys = sorted(d, key=lambda x: d[x], reverse=True)
+        return sorted_keys[:k]
