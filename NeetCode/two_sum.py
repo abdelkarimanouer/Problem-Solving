cf656d23b3743de43b30d0a76c544280e3a5b5b5
Abdelkarim Anouer <188693436+abdelkarimanouer@users.noreply.github.com>
2026-07-17 09:19:44 +0100

    Refactor twoSum method for improved efficiency

--- NeetCode/two_sum.py
+++ NeetCode/two_sum.py
@@ -1,15 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i = 0
-        while i < len(nums) - 1:
-            sum = nums[i]
-            j = i + 1
-            while j < len(nums):
-                sum += nums[j]
-                if sum == target:
-                    return [i, j]
-                else:
-                    sum -= nums[j]
-                j += 1
-            i += 1
+        seen = {}
+
+        for i, n in enumerate(nums):
+            need = target - n
+            if need in seen:
+                return [seen[need], i]
+            seen[n] = i
+        
         return []
